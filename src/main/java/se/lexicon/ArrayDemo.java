e2d68f732950508ad392e4068053b1cf85205877
Mehrdad Javan <mehrdad.javan@lexicon.se>
2025-03-18 09:49:24 +0100

    Array Lecture Part 1

---
+++ src/main/java/se/lexicon/ArrayDemo.java
@@ -0,0 +1,73 @@
+package se.lexicon;
+
+import java.util.Arrays;
+
+public class ArrayDemo {
+
+    public static void main(String[] args) {
+
+        // datatype[] arrayNane;
+        int[] numbers = new int[3]; // [ 0 , 0 , 0 ]
+        String[] names = new String[3]; // [ null , null , null ]
+        char[] letters = {1, 2, 3, 4, 5}; // [5] // [1, 2, 3, 4, 5]
+
+        ex3();
+
+
+    }
+
+    public static void ex1() {
+        String[] names = new String[3]; // [ "Fredrik" , "Markus" , null ]
+        System.out.println(names.length); // 3
+        System.out.println("------------------");
+        names[0] = "Fredrik";
+        names[1] = "Markus";
+        names[names.length - 1] = "Jonas";
+
+        System.out.println(names[1]);
+        System.out.println(names[0]);
+        //System.out.println(names[10]); // Output : Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 10 out of bounds for length 3
+    }
+
+    public static void ex2() {
+        int[] numbers = {5, 2, 3, 4, 7};
+        for (int i = 0; i < numbers.length; i++) {
+            if (i == 2) {
+                numbers[i] = 200;
+            }
+            System.out.println("index [" + i + "] = " + numbers[i]);
+        } // {5, 2, 200, 4, 7}
+
+        System.out.println("-------------------");
+        for (int number : numbers) { // {5, 2, 200, 4, 7}
+            if (number == 200) {
+                number = 5000; // {5, 2, 5000, 4, 7}
+            }
+            System.out.println(number); // {5, 2, 5000, 4, 7}
+        }
+
+        System.out.println("----------------");
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    public static void ex3(){
+        int[] originalArray = {2, 5, 8, 1, 9}; // [2, 5, 8, 1, 9]
+        System.out.println("Original Array:");
+        System.out.println(Arrays.toString(originalArray));
+
+        //  [2, 5, 8, 1, 9, 100]
+        int[] newArray = new int[originalArray.length + 1]; // [0 , 0 , 0 , 0 , 0 , 0]
+
+        for(int i = 0  ;  i< originalArray.length ; i++ ){
+            newArray[i] = originalArray[i];
+        } // [2 , 5 , 8 , 1 , 9 , 0]
+        newArray[newArray.length -1] = 100; // [2 , 5 , 8 , 1 , 9 , 100]
+
+        System.out.println("Expanded Array:");
+        System.out.println(Arrays.toString(newArray));
+
+    }
+
+
+
+}
